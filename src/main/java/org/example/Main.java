2501eaa4fe9d2015fd85ab34986e863f99fcdd5c
Nikita U <nikitajc@mail.ru>
2023-06-24 14:40:32 +0300

    solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -40,22 +40,6 @@ public class Main {
                     space++;
                 }
             }
-
-//        for (int i = 1; i <= 2; i++) {
-//            System.out.println("Iteration 1, circle: " + i);
-//            for (int j = 1; j <= 3; j++) {
-//                System.out.println("\tIteration 2, circle: " + j);
-//                for (int k = 1; k <= 4; k++) {
-//                    System.out.println("\t\tIteration 3, circle: " + k);
-//                    for (int l = 1; l <=5; l++) {
-//                        System.out.print("хуй");
-//                        hui = symbol.repeat(number);
-//                        System.out.println(hui);
-//                        number++;
-//                    }
-//                    System.out.println();
-//                }
-//            }
         }
     }
 }
